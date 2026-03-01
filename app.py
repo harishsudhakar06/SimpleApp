fc6931eda559a0ef4e414d229f6afc39937d36cc
harishsudhakar06 <harishsudhakar0066@gmail.com>
2026-03-01 12:47:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 print("Hello, this is version 1")
+//git init
+git add .
+git commit -m "Initial commit"
+git remote add origin <repo-link>
+git branch -M main
+git push -u origin main
+
+git checkout -b feature-branch
+# modify file
+git add .
+git commit -m "Added new feature"
+git push origin feature-branch
+
+git checkout main
+git merge feature-branch
+git push origin main//
